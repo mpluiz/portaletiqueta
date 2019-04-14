eb17a1931435b325ee3ea897f118ea56a5213b33
mpluiz <marcospaulo.luiz@gmail.com>
2019-04-14 14:33:11 -0300

    Coleta de informações por categoria agora é feita pelo nome da categoria.

--- query.php
+++ query.php
@@ -1,44 +1,41 @@
 <?php
   global $wpdb;
 
-  $query = array('category' => '2', 'orderby' => 'date', 'order' => 'ASC', 'posts_per_page' => '-1'); 
+  $query = array('category_name' => 'banners', 'orderby' => 'date', 'order' => 'ASC', 'posts_per_page' => '-1'); 
   $banners = get_posts($query);
 
-  $query = array('category' => '3', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  $query = array('category_name' => 'noticias', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
   $noticias = get_posts($query);
 
-  $query = array('category' => '4', 'orderby' => 'date', 'order' => 'ASC', 'posts_per_page' => '-1'); 
+  $query = array('category_name' => 'publicidades', 'orderby' => 'date', 'order' => 'ASC', 'posts_per_page' => '-1'); 
   $publicidades = get_posts($query);
 
-  $query = array('category' => '5', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '6'); 
+  $query = array('category_name' => 'videos', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '6'); 
   $videos = get_posts($query);
 
-  $query = array('category' => '6', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  $query = array('category_name' => 'eventos', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
   $eventos = get_posts($query);
 
-  $query = array('category' => '7', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  $query = array('category_name' => 'edicoes', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
   $edicoes = get_posts($query);
 
-  $query = array('category' => '8', 'orderby' => 'rand', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  $query = array('category_name' => 'gastronomias', 'orderby' => 'rand', 'order' => 'DESC', 'posts_per_page' => '-1'); 
   $gastronomias = get_posts($query);
 
-  $query = array('category' => '9', 'orderby' => 'rand', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  $query = array('category_name' => 'parceiros', 'orderby' => 'rand', 'order' => 'DESC', 'posts_per_page' => '-1'); 
   $parceiros = get_posts($query);
 
-  $query = array('category' => '11', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
-  $onde_encontrar = get_posts($query);
-
-  $query = array('category' => '12', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  $query = array('category_name' => 'promocoes', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
   $promocoes = get_posts($query);
 
-  $query = array('category' => '13', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
-  $radares = get_posts($query);
+  // $query = array('category_name' => 'radares', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  // $radares = get_posts($query);
 
-  $query = array('category' => '14', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
-  $blogs_e_colunas = get_posts($query);
+  // $query = array('category_name' => '14', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  // $blogs_e_colunas = get_posts($query);
 
-  $query = array('category' => '15', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
-  $publicidade_personalizada = get_posts($query);
+  // $query = array('category_name' => '15', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => '-1'); 
+  // $publicidade_personalizada = get_posts($query);
 
   $users = get_users();
   shuffle($users);
