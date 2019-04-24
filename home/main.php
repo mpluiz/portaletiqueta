1fa7a0eb434dcfd359c73071c152c08381ed4fb0
mpluiz <marcospaulo.luiz@gmail.com>
2019-04-23 23:17:58 -0300

    Reabilitando os modulos da pagina inicial

--- home/main.php
+++ home/main.php
@@ -1,16 +1,15 @@
 <?php
 
-  // include ('layouts/banner.php');
+  include ('layouts/banner.php');
   include ('layouts/ultimasNoticias.php');
   // include ('layouts/encontro_cidade_politica.php');
-  // include ('layouts/social.php');
-  // include ('layouts/gastronomia.php');
-  // include ('layouts/maisPopulares.php');
-  // include ('layouts/videos.php');
-  // include ('layouts/colunistas.php');
-  // include ('layouts/destaques.php');
-  // include ('layouts/eventos.php');
-
+  include ('layouts/social.php');
+  include ('layouts/gastronomia.php');
+  include ('layouts/maisPopulares.php');
+  include ('layouts/videos.php');
+  include ('layouts/colunistas.php');
+  include ('layouts/destaques.php');
+  include ('layouts/eventos.php');
 ?>
 
 
