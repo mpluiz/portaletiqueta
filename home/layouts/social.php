299b722766c2612f9cd74e721be71aa44b4bd87c
mpluiz <marcospaulo.luiz@gmail.com>
2019-05-09 22:03:48 -0300

    Adicionando bloco de  sub noticias sociais

--- home/layouts/social.php
+++ home/layouts/social.php
@@ -1,27 +1,29 @@
 <section id="social">
   <div class="container">
-    
-    <div class="col-md-8">  
-      <a class="title-section" href="/social"><h3>SOCIAL</h3></a>
+
+    <div class="col-md-8">
+      <a class="title-section" href="/social">
+        <h3>SOCIAL</h3>
+      </a>
       <div class="social owl-carousel owl-theme">
-      <?php
-        foreach($noticias as $noticia){
+        <?php
+        foreach ($noticias as $noticia) {
           $id = $noticia->ID;
           $social = get_field('social', $id);
           $category = get_the_category($id)[0];
           $thumbnail = get_thumbnail($id);
-          if($thumbnail != ''){
-            if($social == true){
-              if($category->slug == 'social'){
+          if ($thumbnail != '') {
+            if ($social == true) {
+              if ($category->slug == 'social') {
                 echo '
                   <div class="item"> 
-                    <a class="post" href='.get_permalink($id).'>
-                      <img src='.$thumbnail.'>
-                      <p class="cat-noticia">'.$category->name.'</p>           
+                    <a class="post" href=' . get_permalink($id) . '>
+                      <img src=' . $thumbnail . '>
+                      <p class="cat-noticia">' . $category->name . '</p>           
                     </a>
-                    <a class="title-post" href='.get_permalink($id).'>
-                      <h4>'.$noticia->post_title.'</h4>
-                      <p class="post-description">'.get_field('descricao', $id).'</p>
+                    <a class="title-post" href=' . get_permalink($id) . '>
+                      <h4>' . $noticia->post_title . '</h4>
+                      <p class="post-description">' . get_field('descricao', $id) . '</p>
                     </a>
                   </div>
                 ';
@@ -29,47 +31,78 @@
             }
           }
         }
-      ?>
-      </div>  <!-- /.Carousel -->
-    </div>  <!-- /.Col-8 -->
-        
+        ?>
+      </div> <!-- /.Carousel -->
+
+      <div class="destaque owl-carousel owl-theme m-top">
+        <?php
+
+        foreach ($noticias as $noticia) {
+          $id = $noticia->ID;
+          $social = get_field('social', $id);
+          $category = get_the_category($id)[0];
+          $thumbnail = get_thumbnail($id);
+          if ($thumbnail != '') {
+            if ($category->slug == 'social') {
+              echo '
+                        <div class="item"> 
+                          <a class="post" href=' . get_permalink($id) . '>
+                            <img src=' . $thumbnail . '>
+                            <p class="cat-noticia">'.$category->name.'</p>           
+                          </a>
+
+                          <a class="title-post" href='.get_permalink($id).'>
+                            <h4>'.$noticia->post_title.'</h4>
+                          </a>
+                        </div>
+                      ';
+            }
+          }
+        }
+        ?>
+      </div>
+      <a class="btn btn-mais" href="/social">Veja +</a>
+    </div> <!-- /.Col-8 -->
+
     <div class="col-md-4 publicidade-lateral">
-      <div class="title-publicidade"><h3>PUBLICIDADE</h3></div>
+      <div class="title-publicidade">
+        <h3>PUBLICIDADE</h3>
+      </div>
       <?php
-        foreach($publicidades as $publicidade){
-          $id = $publicidade->ID;
+      foreach ($publicidades as $publicidade) {
+        $id = $publicidade->ID;
 
-          $date = get_field('ativo_ate', $id);
-          $today = date('Ymd');
-          
-          if ($today <= $date){
+        $date = get_field('ativo_ate', $id);
+        $today = date('Ymd');
 
-            $ativo = get_field('ativo', $id, true);
-            $duplo = get_field('segunda_imagem',$id, true);
-            $posicao = get_field('posicao', $id);
+        if ($today <= $date) {
 
-            if($ativo == true){
-              if($posicao == 'social_lateral'){
-                if($duplo != false){
-                  echo '<div class="col-md-12 img-1">
-                          <img src='.get_field('imagem',$publicidade->ID, true).'>
+          $ativo = get_field('ativo', $id, true);
+          $duplo = get_field('segunda_imagem', $id, true);
+          $posicao = get_field('posicao', $id);
+
+          if ($ativo == true) {
+            if ($posicao == 'social_lateral') {
+              if ($duplo != false) {
+                echo '<div class="col-md-12 img-1">
+                          <img src=' . get_field('imagem', $publicidade->ID, true) . '>
                         </div>
                         <div class="col-md-12 img-2">
-                          <img src='.get_field('segunda_imagem',$publicidade->ID, true).'>
+                          <img src=' . get_field('segunda_imagem', $publicidade->ID, true) . '>
                         </div>
                       ';
-                }else{
-                  echo '<div class="col-md-12 img-full">
-                          <img src='.get_field('imagem',$publicidade->ID, true).'>
+              } else {
+                echo '<div class="col-md-12 img-full">
+                          <img src=' . get_field('imagem', $publicidade->ID, true) . '>
                         </div>
                       ';
-                }
               }
             }
           }
         }
+      }
       ?>
-    </div>  <!-- /.Col-4 -->
+    </div> <!-- /.Col-4 -->
 
-  </div>  <!-- /.Container -->
+  </div> <!-- /.Container -->
 </section>
