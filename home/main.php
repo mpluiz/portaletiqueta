e6b0bdb9181334d074b1318bddfff27755d545a5
mpluiz <marcospaulo.luiz@gmail.com>
2019-07-03 00:01:00 -0300

    Incluindo modulo de colunistas acima do tv etiqueta

--- home/main.php
+++ home/main.php
@@ -6,8 +6,8 @@
   include ('layouts/social.php');
   include ('layouts/gastronomia.php');
   include ('layouts/maisPopulares.php');
-  include ('layouts/videos.php');
   // include ('layouts/colunistas.php');
+  include ('layouts/videos.php');
   // include ('layouts/destaques.php');
   include ('layouts/eventos.php');
 ?>
