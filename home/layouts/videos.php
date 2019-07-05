a6a08da70779e7ddbe02f53a587a2edd2fa0b1d0
mpluiz <marcospaulo.luiz@gmail.com>
2019-07-05 12:19:05 -0300

    Adaptando modulo tv etiqueta para o formato mobile responsivo

--- home/layouts/videos.php
+++ home/layouts/videos.php
@@ -1,39 +1,42 @@
 <section id="videos-home">
   <div class="container">
     <h3 style="color: #fff; font-weight: 700;">TV ETIQUETA</h3>
-    <div class="col-md-12 videos no-padding">
+    <div class="videos row">
 
-      <div class="col-md-8 video-principal no-padding">
-      <?php 
-        foreach ($videos as $video):
-          if (get_field('video_principal', $video->ID) == true):
-      ?>
-      <iframe id="video-principal" width="100%" height="475px" src="https://www.youtube.com/embed/<?php echo get_field('link_do_video', $video->ID) ?>?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-      <?php 
+      <div class="col-md-8 col-xs-12 video-principal">
+        <?php
+        foreach ($videos as $video) :
+          if (get_field('video_principal', $video->ID) == true) :
+            ?>
+            <iframe id="video-principal" width="100%" src="https://www.youtube.com/embed/<?php echo get_field('link_do_video', $video->ID) ?>?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+          <?php
           endif;
         endforeach;
-      ?>
+        ?>
       </div>
 
-      <div class="col-md-4 no-padding lista-videos">
+      <div class="col-md-4 col-xs-12 lista-videos">
         <span class="list-group-item title">ÚLTIMOS VÍDEOS</span>
         <?php
-          foreach ($videos as $video):
-            if (get_field('video_principal', $video->ID) != true):
+        foreach ($videos as $video) :
+          if (get_field('video_principal', $video->ID) != true) :
+            ?>
+            <button type="button" class="list-group-item title-video" value="https://www.youtube.com/embed/<?php echo get_field('link_do_video', $video->ID) ?>?rel=0&amp;showinfo=0">
+              <img class="col-md-4 col-xs-12" src="http://i1.ytimg.com/vi/<?php echo get_field('link_do_video', $video->ID) ?>/hqdefault.jpg">
+              <span class="col-md-8 col-xs-12"><?php echo $video->post_title ?></span>
+            </button>
+          <?php
+          endif;
+        endforeach;
         ?>
-        <button type="button" class="list-group-item title-video" value="https://www.youtube.com/embed/<?php echo get_field('link_do_video', $video->ID) ?>?rel=0&amp;showinfo=0"><img class="col-md-4" src="http://i1.ytimg.com/vi/<?php echo get_field('link_do_video', $video->ID)?>/default.jpg"><span class="col-md-8"><?php echo $video->post_title ?></span></button>
-        <?php 
-            endif;
-          endforeach;
-        ?>
-      <div>
+        <div>
 
-    </div>
-  </div>
+        </div>
+      </div>
 </section>
 
 <script>
-  $('button.title-video').click(function(){
+  $('button.title-video').click(function() {
     new_url = $(this).val();
     iframe = document.getElementById('video-principal');
     iframe.src = new_url;
--- home/main.php
+++ home/main.php
@@ -7,8 +7,8 @@
   include ('layouts/gastronomia.php');
   include ('layouts/maisPopulares.php');
   include ('layouts/colunistas.php');
-  echo '<div class="hidden-xs hidden-sm">';
   include ('layouts/videos.php');
+  echo '<div class="hidden-xs hidden-sm">';
   // include ('layouts/destaques.php');
   include ('layouts/eventos.php');
   echo '</div>';
