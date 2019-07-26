c5606122b48714264486183046c0bc9de60f3730
mpluiz <marcospaulo.luiz@gmail.com>
2019-07-26 10:03:46 -0300

    Corrigindo a regra de banner ativo em todas as paginas

--- single.php
+++ single.php
@@ -1,77 +1,82 @@
-<?php 
-      get_header(); 
-      include "query.php";
-?>
 <?php
 
-if (have_posts() ){
+get_header();
+include "query.php";
+
+if (have_posts()) {
 
   $post = get_post();
   $category = get_the_category();
   $category_parent = get_category_parents($category[0]->category_parent);
 
   // Adicionando Banner em todas as single page se estiver ativo.
-  foreach ($banners as $banner){
+
+  echo '<section id="banner">
+        <div class="container">
+        <div class="banner owl-carousel owl-theme">';
+
+  foreach ($banners as $banner) {
+
     $ativo = get_field('ativo_em_todas_as_paginas', $banner->ID);
-    if (($ativo == true) && ($category[0]->slug != 'revista')){
+
+    if (($ativo == true) && ($category[0]->slug != 'revista')) {
+
       $id = $banner->ID;
 
       $date = get_field('ativo_ate', $id);
       $today = date('Ymd');
-      
-      if ($today <= $date){
-      $thumbnail = get_thumbnail($id);
-        if ($thumbnail != ''){
-          echo'<section id="banner">
-                <div class="container">
-                  <div class="banner owl-carousel owl-theme">';
 
-            if (get_field('extender', $id)) {
-              echo '<a class="post extend" href='.get_field('link', $id).' target="_blank">
-                      <img src='.$thumbnail.'>
-                  </a>';
-            } else {
-              echo '<a class="post" href='.get_field('link', $id).' target="_blank">
-                      <img src='.$thumbnail.'>
-                  </a>';
-            }
+      if ($today <= $date) {
+        $thumbnail = get_thumbnail($id);
+        if ($thumbnail != '') {
 
-            echo' </div>
-                 </div>
-              </section>';
+          if (get_field('extender', $id)) {
+            echo '<a class="post extend" href=' . get_field('link', $id) . ' target="_blank">
+                      <img src=' . $thumbnail . '>
+                  </a>';
+          } else {
+            echo '<a class="post" href=' . get_field('link', $id) . ' target="_blank">
+                      <img src=' . $thumbnail . '>
+                  </a>';
+          }
         }
       }
     }
   }
 
-  if(($category[0]->slug == 'noticias') or ($category_parent == 'Noticias/')){
+  echo '</div>
+        </div>
+        </section>';
+
+  if (($category[0]->slug == 'noticias') or ($category_parent == 'Noticias/')) {
     include "single/noticia.php";
   }
 
-  if($category[0]->slug == 'revista'){
+  if ($category[0]->slug == 'revista') {
     include "single/revista.php";
   }
 
-  if($category[0]->slug == 'eventos'){
+  if ($category[0]->slug == 'eventos') {
     include "single/evento.php";
   }
 
-  if($category[0]->slug == 'gastronomias'){
+  if ($category[0]->slug == 'gastronomias') {
     include "single/gastronomia.php";
   }
 
-  if($category[0]->slug == 'promocao'){
+  if ($category[0]->slug == 'promocao') {
     include "single/promocao.php";
   }
 
-  if($category[0]->slug == 'blogs_e_colunas'){
+  if ($category[0]->slug == 'blogs_e_colunas') {
     include "single/colunista.php";
   }
 
   comments_template();
-}else{
-  include ('404.php');
+} else {
+  include('404.php');
 }
-?>
 
-<?php get_footer(); ?>
+get_footer(); 
+
+?>
